0bf69c8b18f284c1e932c2238a8e549696f9d08b
badisalim <badisalim@gmail.com>
2019-08-06 12:47:07 +0200

    update

--- src/app/per-branch/per-branch.component.ts
+++ src/app/per-branch/per-branch.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'})};
 
 @Component({
   selector: 'app-per-branch',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./per-branch.component.css']
 })
 export class PerBranchComponent implements OnInit {
+  HttpClient: any;
+  shop: any;
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
 
   ngOnInit() {
+    this.httpClient.get('http://localhost:3000/shop/products')
+      .subscribe(shop => {
+        console.log(shop);
+        this.shop = shop;
+      });
+  }
+  public calculateBasketTotal(basket) {
+    const productsTotal = basket.products.map(product => product.amount * product.price);
+    return productsTotal.reduce((product1, product2) => product1 + product2);
   }
 
+  public calculateBranchTotal(branch) {
+    return branch.baskets.map(this.calculateBasketTotal).reduce((total1, total2) => total1 + total2);
+  }
+  public calculateShopTotal(shop) {
+
+    const baskets = shop.map(branch => this.calculateBranchTotal(branch));
+    console.log(baskets);
+
+    return baskets.reduce((one, two) => one + two);
+
+  }
 }
+
+
