ac384b01ceb69f7de8146f91d586d2254b4de8a4
badisalim <badisalim@gmail.com>
2019-08-06 12:23:09 +0200

    uptodate

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule,HttpClient } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PerBranchComponent } from './per-branch/per-branch.component';
+import { PerProductComponent } from './per-product/per-product.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { EditProductComponent } from './edit-product/edit-product.component';
+import { LinkProductComponent } from './link-product/link-product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PerBranchComponent,
+    PerProductComponent,
+    AddProductComponent,
+    EditProductComponent,
+    LinkProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClient,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/per-branch/per-branch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-per-branch',
+  templateUrl: './per-branch.component.html',
+  styleUrls: ['./per-branch.component.css']
+})
+export class PerBranchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
